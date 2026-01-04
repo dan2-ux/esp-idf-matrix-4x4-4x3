5630b238a9c49cbf3ab8c27e40fce92321a6fea2
Pham Cong Danh <danhlanhat9@gmail.com>
2026-01-04 18:12:08 +0700

    Update example4x3.cpp

--- example4x3.cpp
+++ example4x3.cpp
@@ -2,32 +2,33 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-// enter the 4 gpio for row into the struct bellow
 struct matrixGPIOr3 row =
     {
         .gpio = {GPIO_NUM_13, GPIO_NUM_12, GPIO_NUM_14, GPIO_NUM_27},
         .mode = GPIO_MODE_INPUT,
 };
-
-// enter the 3 gpio for col into the struct bellow
 struct matrixGPIOc3 col =
     {
-        .gpio = {GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_33},
+        .gpio = {GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_33, GPIO_NUM_32},
         .mode = GPIO_MODE_OUTPUT,
 };
+
+struct buzzerGPIO3 buzzer =
+    {
+        .pin = GPIO_NUM_4};
 
 void printOutMatrix(void *arg)
 {
     while (1)
     {
-        displayMATRIX3(&row, &col);
+        displayMATRIX3(&row, &col, &buzzer);
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 extern "C" void app_main()
 {
-    setupMATRIXGPIO3(&row, &col);
+    setupMATRIXGPIO3(&row, &col, &buzzer);
 
-    xTaskCreatePinnedToCore(printOutMatrix3, "print out the pressing matrix value", 2048, NULL, 5, NULL, 1);
+    xTaskCreatePinnedToCore(printOutMatrix, "print out the pressing matrix value", 2048, NULL, 5, NULL, 1);
 }
